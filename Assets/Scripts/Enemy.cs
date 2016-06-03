d0098f25eb6d8e98b4f4c3201bde934d1ab6937c
Brian Cain <brian.cain@puppetlabs.com>
2016-06-03 16:26:01 -0700

    Add beginnings to enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+  private Rigidbody2D rb;
+
+  // Use this for initialization
+  void Start () {
+  }
+  // Update is called once per frame
+  void Update () {
+  }
+}
